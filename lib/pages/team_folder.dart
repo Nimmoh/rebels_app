7857097b99a25c5cd460f2bf01cdde3881fb8fe8
joy <nimmohndabari@gmail.com>
2024-01-29 17:40:37 +0300

    riottersss

--- lib/pages/team_folder.dart
+++ lib/pages/team_folder.dart
@@ -13,16 +13,17 @@ class TeamFolderPage extends StatefulWidget {
 
 class _TeamFolderPageState extends State<TeamFolderPage> {
   double availableScreenWidth = 0;
+
   @override
   Widget build(BuildContext context) {
     availableScreenWidth = MediaQuery.of(context).size.width -50;
     return Scaffold(
-      backgroundColor: Colors.lime,
+      backgroundColor: Colors.grey,
       body: Column(children: [
         Container(
           padding: const EdgeInsets.symmetric(vertical: 25,horizontal: 25),
           height: 170,
-          decoration: BoxDecoration(color: Colors.blue.shade800),
+          decoration: BoxDecoration(color: Color.fromARGB(181, 151, 7, 7)),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -33,7 +34,7 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
-              color: Colors.white,
+              color: Colors.black,
             ),
             ),
             Text("Teams",
@@ -134,35 +135,27 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
           ),
         ),
        
-        SizedBox(height: 15,),
+        const SizedBox(height: 15,),
 
         const Divider(height: 20,),
         Expanded(
           child: ListView(
-            padding: EdgeInsets.all(25),
-          children: [Text(
+            padding: const EdgeInsets.all(25),
+          children: [const Text(
             "Recently updated",
           style: TextStyle(
             fontSize: 16,
           fontWeight: FontWeight.bold),
           ),
           
-          SizedBox(height: 15,),
+          const SizedBox(height: 15,),
           Row(
             children: [
-              Column(
-                children: [Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.circular(15),),
-                    padding: EdgeInsets.all(30),
-                    height: 80,
-                  child: Image.asset("assets/images/pic1.png"),
-                ),
-                
-                ],
-                
-              )
+              buildFileSizedColumn('sketch', 'desktop','sketch'),
+              SizedBox(width: availableScreenWidth *.15,)
+              buildFileSizedColumn('sketch', 'mobile','sketch'),
+              buildFileSizedColumn('sketch', 'extension','sketch'),
+              
             ],
           )
           ],
@@ -173,6 +166,45 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
       ),
       
     );
+  }
+
+  Column buildFileSizedColumn(String image, String filename, String extension) {
+    return Column(
+              children: [
+                Container(
+                  width: availableScreenWidth *.15,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  height: 50,
+                child: Image.asset('assets/images/$image.jpg'),
+              ),
+              RichText(
+                text: TextSpan(
+                text: filename,
+                style: TextStyle(color: Colors.black,
+                fontSize: 11,
+                fontWeight: FontWeight.w300,
+                ),
+                children: [
+                  TextSpan(
+                    text: extension,
+                    style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 11,
+                ),
+                ),
+                ],
+                ),
+                
+              ),
+
+              ],
+              
+            );
   }
 
   Column buildFileSizedChart(String title,Color? color, double widthPercentage) {
@@ -189,7 +221,7 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
               const SizedBox(height: 20),
         
                Text(title,
-              style: TextStyle(fontSize: 10,
+              style: const TextStyle(fontSize: 10,
               fontWeight: FontWeight.bold),
               ),
               ],
